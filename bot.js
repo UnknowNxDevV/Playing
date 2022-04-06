0a8bc8c3299f876146d57bd2a2e44a50375e5114
XalaAbo <76926045+XalaAbo@users.noreply.github.com>
2022-04-07 00:24:03 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const { Client, Intents, Collection } = require('discord.js');
+const express = require("express");
+const client = new Client({ intents: [
+Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+Intents.FLAGS.GUILD_MEMBERS,
+] 
+  });
+
+
+
+
+client.on('ready', async () => { console.log(`The`,client.user.tag,`is Online`);})
+
+client.on('ready', async () => {
+client.user.setPresence({status: "dnd" } )
+client.user.setActivity( {type: "PLAYING", url: "https://www.twitch.tv/@#£"}, {name: "Bot Under Development"} ) 
+
+   })
+   
+client.login("")
