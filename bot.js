2cb047a9fe455278051b29cdef683e3080d97ba9
XalaAbo <76926045+XalaAbo@users.noreply.github.com>
2022-08-01 11:56:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,4 +16,4 @@ client.user.setActivity( {type: "PLAYING", url: "https://www.twitch.tv/@#£"}, {
 
    })
    
-client.login("ODAyOTI3NTMzMDcwMzUyNDI1.GB87Ut.5_WBWO-ul-Gse4dY8b7gDrZ7nSrskRwoQeYGlw")
+client.login("")
